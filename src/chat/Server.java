fc2cd2adbbc50d7b20680a38b280225fae30f5f7
ustimenv <ustimenkovlad@gmail.com>
2020-04-14 13:15:24 +0100

    Added TLSv1.2 support

--- src/chat/Server.java
+++ src/chat/Server.java
@@ -2,25 +2,51 @@ package chat;
 
 import java.io.*;
 import java.net.*;
+import java.security.*;
+import java.security.cert.CertificateException;
 import java.util.ArrayList;
+import com.sun.net.httpserver.*;
+import javax.net.ssl.*;
+
 
 public class Server
 {
-	ServerSocket serverSocket;
-	Socket receivingSocket;
-	private final int ownPort;
+	SSLServerSocket serverSocket;
+	SSLSocket receivingSocket;
+	
+//	ServerSocket serverSocket;
+//	Socket receivingSocket;
 	
 	public Server()
 	{
-		ownPort = 50000;
 		try
 		{
-			serverSocket = new ServerSocket(ownPort);
-		}catch(IOException e)
+//			this.serverSocket = new ServerSocket(50000);
+			System.setProperty("javax.net.debug", "ssl");
+			
+			KeyStore keyStore = KeyStore.getInstance("JKS");
+			InputStream is = new BufferedInputStream(new FileInputStream("my_keystore"));
+			keyStore.load(is, "123456".toCharArray());
+			
+			KeyManagerFactory kmf = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
+			kmf.init(keyStore, "123456".toCharArray());
+			
+			SSLContext sslContext = SSLContext.getInstance("TLSv1.2");
+			sslContext.init(kmf.getKeyManagers(), null, null);
+			
+			SSLServerSocketFactory serverSocketFactory = (SSLServerSocketFactory) sslContext.getServerSocketFactory();
+			
+			this.serverSocket = (SSLServerSocket) serverSocketFactory.createServerSocket(50000);
+			this.serverSocket.setNeedClientAuth(false);
+			this.serverSocket.setWantClientAuth(false);
+		}catch(IOException | KeyStoreException | NoSuchAlgorithmException | CertificateException | UnrecoverableKeyException | KeyManagementException e)
 		{
 			e.printStackTrace();
 		}
 	}
+	
+	
+	
 	private void listen()
 	{
 		ClientOperationsExecutor x = new ClientOperationsExecutor();
@@ -31,19 +57,30 @@ public class Server
 			try
 			{
 				System.out.println("Awaiting input");
-				receivingSocket = serverSocket.accept();
+				receivingSocket = (SSLSocket) serverSocket.accept();;
 				System.out.println(receivingSocket.getInetAddress() + " " + receivingSocket.getLocalAddress() + " " + receivingSocket.getPort() + " " + receivingSocket .getLocalPort());
-				Thread.sleep(1000);									//DEBUG ONLY, emulate network latency
 				
+				
+//				for(String s : receivingSocket.getEnabledCipherSuites())
+//					System.out.print(s + "\t");
+//				System.out.println();
+//				for(String s : serverSocket.getEnabledCipherSuites())
+//					System.out.print(s + "\t");
+				
+				
+				
+				Thread.sleep(1000);									//DEBUG ONLY, emulate network latency
+
 				System.out.println("Received");
 				input = bufferedReaderToString( new BufferedReader(new InputStreamReader(receivingSocket.getInputStream())));
 				flag = Character.getNumericValue(input.charAt(0));
+				System.out.println(input);
 			}catch(IOException | InterruptedException e){ e.printStackTrace();}
-			
+
 			try
 			{
 				ArrayList<MessageSender> responses = new ArrayList<>();
-				
+
 				switch (flag) {
 					case 1:
 						responses = x.executeOperation(new LoginOperation(input, receivingSocket.getInetAddress()));
@@ -81,6 +118,7 @@ public class Server
 	public static void main(String[] args)
 	{
 		Server server = new Server();
+		
 		server.listen();
 	}
 }
